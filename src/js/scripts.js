11f877fd2b1d3cae08ad999e6d22045464c252ee
Alexander <a.vasilevec@imedia.by>
2024-02-20 12:36:14 +0300

    Development

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,6 @@
 import Lenis from '@studio-freight/lenis';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-
-/* import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules'; */
-
 /* Base scripts */
 
 /* Проверка на safari */
@@ -42,76 +37,6 @@ if (isMobile) {
   document.querySelector('html').classList.add('is-mobile');
 }
 
-/* Проверка ширины экрана */
-/* function checkInnerWidth(width) {
-  if (window.innerWidth <= width) {
-    return true;
-  } else {
-    return false;
-  }
-} */
-
-/* POPUPS */
-/* $('[data-popup]').click(function (e) {
-  e.preventDefault();
-  let popup = $(this).data('popup');
-  $(`.${popup}_popup`).fadeIn();
-  $('body,html').addClass('noskroll');
-});
-
-$('.popup__close').click(function () {
-  let popup = $(this).closest('.popup');
-  $(popup).fadeOut();
-  $('body,html').removeClass('noskroll');
-}); */
-
-/* FORM ERRORS */
-/* 
-const contactForm = document.querySelectorAll('.contacts-form');
-contactForm.forEach((el) => {
-  el.addEventListener('submit', function (event) {
-    let errors = 0;
-
-    const fields = el.querySelectorAll('.input');
-    fields.forEach((field) => {
-      const error = field.closest('.form-field-box').querySelector('.field-error');
-      const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-      if (field.classList.contains('is-required') && !field.value) {
-        errors++;
-        field.classList.add('is-error');
-        if (error) {
-          error.classList.add('is-visible');
-          error.innerHTML = 'Field is required';
-        }
-      } else if (field.classList.contains('is-email') && !field.value.match(mailformat)) {
-        errors++;
-        field.classList.add('is-error');
-        if (error) {
-          error.classList.add('is-visible');
-          error.innerHTML = 'The field is filled incorrectly';
-        }
-      }
-    });
-
-    if (errors) {
-      event.preventDefault();
-      return false;
-    }
-  });
-});
-
-const fields = document.querySelectorAll('form .input');
-fields.forEach((el) => {
-  const error = el.closest('.form-field-box').querySelector('.field-error');
-  el.addEventListener('input', function (event) {
-    el.classList.remove('is-error');
-    if (error) {
-      error.classList.remove('is-visible');
-      error.innerHTML = '';
-    }
-  });
-}); */
-
 /* Lazy load */
 /* var observer = lozad('[data-lazysrc]', {
   threshold: 0.1,
@@ -130,10 +55,10 @@ var pictureObserver = lozad('.lozad', {
   threshold: 0.1
 })
 pictureObserver.observe() */
+
 gsap.registerPlugin(ScrollTrigger);
 
 window.startLenis = () => {
-  console.log('start');
   window.lenis = new Lenis();
   document.querySelector('html').classList.remove('noskroll');
   document.querySelector('body').classList.remove('noskroll');
@@ -284,5 +209,86 @@ import { MaskInput } from 'maska';
     });
   });
 })();
+const toastData = {
+  success: {
+    icon: '<svg viewBox="0 0 32 32" width="1.25rem" height="1.25rem" style="overflow: visible;"><circle cx="16" cy="16" r="0" fill="#34C759"><animate attributeName="opacity" values="0; 1; 1" dur="0.35s" begin="100ms" fill="freeze" calcMode="spline" keyTimes="0; 0.6; 1" keySplines="0.25 0.71 0.4 0.88; .59 .22 .87 .63"></animate><animate attributeName="r" values="0; 17.5; 16" dur="0.35s" begin="100ms" fill="freeze" calcMode="spline" keyTimes="0; 0.6; 1" keySplines="0.25 0.71 0.4 0.88; .59 .22 .87 .63"></animate></circle><circle cx="16" cy="16" r="12" opacity="0" fill="#34C759"><animate attributeName="opacity" values="1; 0" dur="1s" begin="350ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0 0.0 0.2 1"></animate><animate attributeName="r" values="12; 26" dur="1s" begin="350ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0 0.0 0.2 1"></animate></circle><path fill="none" stroke-width="4" stroke-dasharray="22" stroke-dashoffset="22" stroke-linecap="round" stroke-miterlimit="10" d="M9.8,17.2l3.8,3.6c0.1,0.1,0.3,0.1,0.4,0l9.6-9.7" stroke="#FCFCFC"><animate attributeName="stroke-dashoffset" values="22;0" dur="0.25s" begin="250ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0, 0.0, 0.58, 1.0"></animate></path></svg>',
+  },
+  error: {
+    icon: '<svg viewBox="0 0 32 32" width="1.25rem" height="1.25rem" style="overflow: visible;"><circle cx="16" cy="16" r="0" fill="#FF3B30"><animate attributeName="opacity" values="0; 1; 1" dur="0.35s" begin="100ms" fill="freeze" calcMode="spline" keyTimes="0; 0.6; 1" keySplines="0.25 0.71 0.4 0.88; .59 .22 .87 .63"></animate><animate attributeName="r" values="0; 17.5; 16" dur="0.35s" begin="100ms" fill="freeze" calcMode="spline" keyTimes="0; 0.6; 1" keySplines="0.25 0.71 0.4 0.88; .59 .22 .87 .63"></animate></circle><circle cx="16" cy="16" r="12" opacity="0" fill="#FF3B30"><animate attributeName="opacity" values="1; 0" dur="1s" begin="320ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0 0.0 0.2 1"></animate><animate attributeName="r" values="12; 26" dur="1s" begin="320ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0 0.0 0.2 1"></animate></circle><path fill="none" stroke-width="4" stroke-dasharray="9" stroke-dashoffset="9" stroke-linecap="round" d="M16,7l0,9" stroke="#FFFFFF"><animate attributeName="stroke-dashoffset" values="9;0" dur="0.2s" begin="250ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0, 0.0, 0.58, 1.0"></animate></path><circle cx="16" cy="23" r="2.5" opacity="0" fill="#FFFFFF"><animate attributeName="opacity" values="0;1" dur="0.25s" begin="350ms" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.0, 0.0, 0.58, 1.0"></animate></circle></svg>',
+  },
+};
+let toastTimeout = null;
+function activateToast(text, type, delay) {
+  const toast = document.querySelector('.custom-toast');
+  if (!toast) return false;
+
+  const toastIcon = toast.querySelector('.custom-toast__icon');
+  const toastText = toast.querySelector('.custom-toast__text');
+
+  toastIcon.innerHTML = toastData[type].icon;
+  toastText.innerHTML = text;
+  toast.classList.add('is-enter');
+
+  clearTimeout(toastTimeout);
+  toastTimeout = setTimeout(() => {
+    toast.classList.remove('is-enter');
+    toast.classList.add('is-out');
+    setTimeout(() => {
+      toast.classList.remove('is-out');
+      toastIcon.innerHTML = '';
+      toastText.innerHTML = '';
+    }, 300);
+  }, delay);
+}
+
+/* FORM ERRORS */
+const validateForms = document.querySelectorAll('.js-validate-form');
+validateForms.forEach((el) => {
+  el.addEventListener('submit', function (event) {
+    let errors = 0;
+    const fields = el.querySelectorAll('.form-input');
+    fields.forEach((field) => {
+      const input = field.querySelector('.input');
+      if (input) {
+        const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if (field.classList.contains('is-checkbox') && field.classList.contains('is-required') && !input.checked) {
+          errors++;
+          field.classList.add('is-error');
+        } else if (field.classList.contains('is-email') && input.value && !input.value.match(mailformat)) {
+          errors++;
+          field.classList.add('is-error');
+        } else if (
+          field.classList.contains('is-required') &&
+          field.classList.contains('is-phone') &&
+          !field.classList.contains('is-valid')
+        ) {
+          errors++;
+          field.classList.add('is-error');
+        } else if (field.classList.contains('is-required') && !input.value) {
+          errors++;
+          field.classList.add('is-error');
+        }
+      }
+    });
+
+    if (errors) {
+      event.preventDefault();
+      activateToast('Заполните обязательные поля', 'error', 2000);
+      return false;
+    }
+  });
+});
+
+const fields = document.querySelectorAll('.js-validate-form .form-input');
+fields.forEach((field) => {
+  const input = field.querySelector('.input');
+  if (input) {
+    input.addEventListener('input', function () {
+      field.classList.remove('is-error');
+    });
+  }
+});
+
 import './import/modules';
 import './import/components';
